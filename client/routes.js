0a175d7336b0da855c729c35ce929d9ec3d9098a
Betsy Gottesman <eightamps@gmail.com>
2017-05-02 22:15:40 -0700

    refactoring maps

--- client/routes.js
+++ client/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Router, Route, IndexRoute} from 'react-router';
 import {App} from './App';
-import {Map} from './Map';
+import {MapContainer} from './MapContainer';
 import {Review} from './Review';
 import {Detail} from './Detail';
 
 export default  (
     <Router>
         <Route path="/" component={App}>
-            <IndexRoute component={Map}/>
-            <Route path="/map" component={Map}/>
+            <IndexRoute component={MapContainer}/>
+            <Route path="/map" component={MapContainer}/>
             <Route path="/review(/:id)" component={Review}/>
             <Route path="/detail/:id" component={Detail}/>
         </Route>
