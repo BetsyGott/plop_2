a0b7dfebcbf969eefe958fb30ca6b96f94a97729
Betsy Gottesman <eightamps@gmail.com>
2017-05-02 21:32:44 -0700

    adding google api scripts

--- client/App.js
+++ client/App.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
-import {Router} from 'react-router';
+import {Router, Link} from 'react-router';
+import styles from './App.css';
 
 export class App extends Component {
 
   render() {
     return (
         <div>
-            <h2>This is the main app</h2>
+          <div className="ui top attached menu">
+            <Link to="/">
+              <div className="item">
+                <img src={require('assets/water.png')}/>
+              </div>
+            </Link>
+            <div className="item">
+              <div className="ui primary button">
+                <Link to="/review"><span className={styles.headerlink}>Write a Review</span></Link>
+              </div>
+            </div>
+            <div className="right header item"> Plop </div>
+          </div>
             {this.props.children}
         </div>
     );
