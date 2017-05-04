23210e0b1d3b7c68f8c914c38a16370e6a78eda5
Betsy Gottesman <eightamps@gmail.com>
2017-05-03 22:09:08 -0700

    commiting

--- client/Map.js
+++ client/Map.js
@@ -1,44 +1,76 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import GoogleMapReact from 'google-map-react';
 
-export class Map extends Component {
+const K_WIDTH = 40;
+const K_HEIGHT = 40;
 
-  componentDidUpdate (prevProps, prevState){
-    if (prevProps.google !== this.props.google) {
-      this.loadMap();
-    }
+const greatPlaceStyle = {
+  // initially any map object has left top corner at lat lng coordinates
+  // it's on you to set object origin to 0,0 coordinates
+  position: 'absolute',
+  width: K_WIDTH,
+  height: K_HEIGHT,
+  left: -K_WIDTH / 2,
+  top: -K_HEIGHT / 2,
+
+  border: '5px solid #f44336',
+  borderRadius: K_HEIGHT,
+  backgroundColor: 'white',
+  textAlign: 'center',
+  color: '#3f51b5',
+  fontSize: 16,
+  fontWeight: 'bold',
+  padding: 4
+};
+
+class MyGreatPlace extends Component {
+  static propTypes = {
+    text: PropTypes.string
+  };
+
+  static defaultProps = {};
+
+  render() {
+    return (
+        <div style={greatPlaceStyle}>
+          {this.props.text}
+        </div>
+    );
   }
+}
 
-  componentDidMount() {
-    this.loadMap();
-  }
+class Map extends Component {
+  static propTypes = {
+    center: PropTypes.array,
+    zoom: PropTypes.number,
+    greatPlaceCoords: PropTypes.any
+  };
 
-  loadMap () {
-    if (this.props && this.props.google) {
-      // google is available
-      const {google} = this.props;
-      const maps = google.maps;
+  static defaultProps = {
+    center: [59.938043, 30.337157],
+    zoom: 9,
+    greatPlaceCoords: {lat: 59.724465, lng: 30.080121}
+  };
 
-      const mapRef = this.refs.map;
-      const node = ReactDOM.findDOMNode(mapRef);
-
-      let zoom = 14;
-      let lat = 37.774929;
-      let lng = -122.419416;
-      const center = new maps.LatLng(lat, lng);
-      const mapConfig = Object.assign({}, {
-        center: center,
-        zoom: zoom
-      });
-      this.map = new maps.Map(node, mapConfig);
-    }
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
-        <div ref="map">
-          Loading map....
-        </div>
-    )
+
+          <GoogleMapReact
+               apiKey={process.env.GAPI_KEY}
+               center={this.props.center}
+               zoom={this.props.zoom}>
+            <MyGreatPlace lat={59.955413} lng={30.337844} text={'A'} />
+            <MyGreatPlace {...this.props.greatPlaceCoords} text={'B'} />
+          </GoogleMapReact>
+
+
+    );
   }
 }
+
+export default Map;
--- client/routes.js
+++ client/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Router, Route, IndexRoute} from 'react-router';
 import {App} from './App';
-import {MapContainer} from './MapContainer';
+import {Map} from './Map';
 import {Review} from './Review';
 import {Detail} from './Detail';
 
 export default  (
     <Router>
         <Route path="/" component={App}>
-            <IndexRoute component={MapContainer}/>
-            <Route path="/map" component={MapContainer}/>
+            <IndexRoute component={Map}/>
+            <Route path="/map" component={Map}/>
             <Route path="/review(/:id)" component={Review}/>
             <Route path="/detail/:id" component={Detail}/>
         </Route>
