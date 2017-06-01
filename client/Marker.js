c9ff70d168e5c9800502796c66432678078fc6aa
Betsy Gottesman <eightamps@gmail.com>
2017-05-31 22:43:51 -0700

    tweaked marker display

--- client/Marker.js
+++ client/Marker.js
@@ -1,14 +1,10 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { Button, Popup, Rating } from 'semantic-ui-react';
+import { Button, Popup, Rating, Icon } from 'semantic-ui-react';
 
 const popupStyle = {
   textAlign: 'center',
   fontSize: '.9em'
-};
-
-const addressStyle = {
-  padding: '0 0 8px 0'
 };
 
 class Marker extends Component {
@@ -29,13 +25,25 @@ class Marker extends Component {
     <Popup hoverable trigger={<Button basic circular color='green' icon='map pin' />}
            position="top center"
            style={popupStyle}>
-      <Popup.Header>{this.props.name}</Popup.Header>
+      <Popup.Header> {this.props.name} </Popup.Header>
       <Popup.Content>
-        <div style={addressStyle}>
-          <p>{this.props.address.street}<br/>
-          {this.props.address.city},{this.props.address.state} {this.props.address.zip}</p>
+
+          <span>3.2</span>
+        <div style={{display: 'inline-block',margin: '0 5px'}}>
+          <Rating size="tiny" disabled icon='star' defaultRating={3} maxRating={5} />
         </div>
-        <Rating icon='star' defaultRating={3} maxRating={4} />
+          <span>(15)</span>
+        <br/>
+          <div style={{marginTop: '5px'}}>
+            <Popup trigger={<Icon circular name='handicap' />}
+                   size='tiny' content="Handicap stall" position="top center" inverted/>
+            <Popup trigger={<Icon circular name='music' />}
+                   size='tiny' content="Music" position="top center" inverted/>
+            <Popup trigger={<Icon circular name='users' />}
+                   size='tiny' content="Automatic hand dryers" position="top center" inverted/>
+            <Popup trigger={<Icon circular name='users' />}
+                   size='tiny' content="Paper towels" position="top center" inverted/>
+          </div>
       </Popup.Content>
     </Popup>
 
